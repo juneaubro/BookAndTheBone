b97a2dd1d675b074d39e2c7e0d0200d32649f27a
juneaubro <55204879+juneaubro@users.noreply.github.com>
2019-11-13 21:03:52 -0500

    Dog movement completed

--- BookAndTheBone/Assets/Scripts/DogController.cs
+++ BookAndTheBone/Assets/Scripts/DogController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class DogController : MonoBehaviour
 {
+    public float runSpeed;
+    public int jumpPower;
+    public float jumpCooldown;
 
     private float dirX;
-    private float runSpeed = 10f;
     private Animator anim;
+    private Collision2D hit;
     private bool facingRight;
+    
+    private float counter = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +25,7 @@ public class DogController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        counter += Time.deltaTime;
 
         dirX = Input.GetAxisRaw("Horizontal") * runSpeed * Time.deltaTime;
 
@@ -27,7 +33,13 @@ public class DogController : MonoBehaviour
 
         Flip(dirX);
 
-        if(dirX != 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("bark"))
+        if(Input.GetKeyDown(KeyCode.W) && counter > jumpCooldown)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower);
+            counter = 0f;
+        }
+
+        if (dirX != 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("bark"))    // If the dog is moving and not barking.
         {
             anim.SetBool("isRunning", true);
         } else
@@ -40,9 +52,26 @@ public class DogController : MonoBehaviour
             anim.SetBool("isRunning", false);
             anim.SetTrigger("hit");
         }
+
+        if(Input.GetKey(KeyCode.S) && !anim.GetCurrentAnimatorStateInfo(0).IsName("bark"))
+        {
+            anim.SetBool("isSitting", true);
+        } else if(Input.GetKeyUp(KeyCode.S))
+        {
+            anim.SetBool("isSitting", false);
+        }
+
+        if (Input.GetKey(KeyCode.W) && !anim.GetCurrentAnimatorStateInfo(0).IsName("bark"))
+        {
+            anim.SetBool("isJumping", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("isJumping", false);
+        }
     }
 
-    private void Flip(float dirX)
+    private void Flip(float dirX)   // To make the dog flip around to face the direction he is going.
     {
         if (dirX > 0 && !facingRight || dirX < 0 && facingRight)
         {
