4ed0fa8e27002a4ec05a343d531c6325e7bf5c4a
juneaubro <55204879+juneaubro@users.noreply.github.com>
2019-11-20 07:48:35 -0500

    Player can open chest. Help text.
    Man can open chest and added a help text.

--- BookAndTheBone/Assets/Scripts/ChestOpen.cs
+++ BookAndTheBone/Assets/Scripts/ChestOpen.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class ChestOpen : MonoBehaviour
 {
-
+    public GameObject book;
+    public GameObject portal;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            book.SetActive(true);
+            portal.SetActive(true);
+        }
     }
 }
--- BookAndTheBone/Assets/Scripts/GameController.cs
+++ BookAndTheBone/Assets/Scripts/GameController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject canvas;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            canvas.SetActive(false);
+        }
     }
 }
